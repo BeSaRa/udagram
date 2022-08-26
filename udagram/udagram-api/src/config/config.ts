cd44237ba643604f10d333c03007adb5bb4c11e2
BeSaRa <besara@windowslive.com>
2022-08-26 03:09:04 +0300

    ignore some of eslint in configuration file

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -1,4 +1,5 @@
 import * as dotenv from "dotenv";
+
 dotenv.config();
 
 // ENV variables 
@@ -13,8 +14,11 @@ export const config = {
   port: Number(process.env.PORT),
   host: process.env.POSTGRES_HOST,
   dialect: "postgres",
+  // eslint-disable-next-line @typescript-eslint/camelcase
   aws_region: process.env.AWS_REGION,
+  // eslint-disable-next-line @typescript-eslint/camelcase
   aws_profile: process.env.AWS_PROFILE,
+  // eslint-disable-next-line @typescript-eslint/camelcase
   aws_media_bucket: process.env.AWS_BUCKET,
   url: process.env.URL,
   jwt: {
