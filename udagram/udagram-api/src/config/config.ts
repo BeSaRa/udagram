c5e7eca97275848b3da0510f8bb681fe8ba194ce
BeSaRa <besara@windowslive.com>
2022-08-28 02:38:22 +0300

    remove the logs

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -1,10 +1,6 @@
 import * as dotenv from "dotenv";
 
 dotenv.config();
-
-console.log("ENV_VARIABLE", process.env);
-
-
 export const config = {
   username: `${process.env.POSTGRES_USERNAME}`,
   password: process.env.POSTGRES_PASSWORD,
--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,10 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-console.log('USER:', config.username)
-console.log('PASSWORD:', config.password)
-console.log('DB:', config.database)
-console.log('HOST:', config.host)
 export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
