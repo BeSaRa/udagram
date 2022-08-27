d5c413522fef731617f702477693f6fe3a223092
BeSaRa <besara@windowslive.com>
2022-08-28 00:58:57 +0300

    check env variable form config

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -3,13 +3,13 @@ import { config } from "./config/config";
 
 console.log('USER:', config.username)
 console.log('PASSWORD:', config.password)
-console.log('HOST:', config.database)
+console.log('DB:', config.database)
+console.log('HOST:', config.host)
 export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
   database: config.database,
   host: config.host,
-
   dialect: "postgres",
   storage: ":memory:",
 });
