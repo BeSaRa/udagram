41b7dc5ba1bef59094da2a226435f9afbf17e5ae
BeSaRa <besara@windowslive.com>
2022-08-28 01:45:04 +0300

    remove db for test purpose

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -5,11 +5,11 @@ console.log('USER:', config.username)
 console.log('PASSWORD:', config.password)
 console.log('DB:', config.database)
 console.log('HOST:', config.host)
-export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
-  dialect: "postgres",
-  storage: ":memory:",
-});
+// export const sequelize = new Sequelize({
+//   username: config.username,
+//   password: config.password,
+//   database: config.database,
+//   host: config.host,
+//   dialect: "postgres",
+//   storage: ":memory:",
+// });
--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -1,19 +1,19 @@
 import * as dotenv from "dotenv";
 import cors from 'cors';
 import express from "express";
-import { sequelize } from "./sequelize";
+// import { sequelize } from "./sequelize";
 
 import { IndexRouter } from "./controllers/v0/index.router";
 
 import bodyParser from "body-parser";
-import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
+// import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
 (async (): Promise<void> => {
   dotenv.config();
 
-  await sequelize.addModels(V0_FEED_MODELS);
-  await sequelize.addModels(V0_USER_MODELS);
-  await sequelize.sync();
+  // await sequelize.addModels(V0_FEED_MODELS);
+  // await sequelize.addModels(V0_USER_MODELS);
+  // await sequelize.sync();
 
   console.log("Database Connected");
 
