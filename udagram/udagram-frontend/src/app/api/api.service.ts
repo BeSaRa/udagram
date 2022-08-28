4f57ff8163a8f520421df8c2c6e80caa6abe757c
BeSaRa <besara@windowslive.com>
2022-08-28 06:18:07 +0300

    test

--- udagram/udagram-frontend/src/app/api/api.service.ts
+++ udagram/udagram-frontend/src/app/api/api.service.ts
@@ -10,7 +10,7 @@ const API_HOST = environment.apiHost;
 })
 export class ApiService {
   httpOptions = {
-    headers: new HttpHeaders({'Content-Type': 'application/json'})
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
   token: string;
@@ -23,7 +23,7 @@ export class ApiService {
   }
 
   static extractData(res: HttpEvent<any>) {
-    return res || { };
+    return res || {};
   }
 
   setAuthToken(token) {
@@ -36,36 +36,37 @@ export class ApiService {
     const req = this.http.get(url, this.httpOptions).pipe(map(ApiService.extractData));
 
     return req
-            .toPromise()
-            .catch((e) => {
-              ApiService.handleError(e);
-              throw e;
-            });
+      .toPromise()
+      .catch((e) => {
+        ApiService.handleError(e);
+        throw e;
+      });
   }
 
   post(endpoint, data): Promise<any> {
     const url = `${API_HOST}${endpoint}`;
     return this.http.post<HttpEvent<any>>(url, data, this.httpOptions)
-            .toPromise()
-            .catch((e) => {
-              ApiService.handleError(e);
-              throw e;
-            });
+      .toPromise()
+      .catch((e) => {
+        ApiService.handleError(e);
+        throw e;
+      });
   }
 
   async upload(endpoint: string, file: File, payload: any): Promise<any> {
+    console.log((await this.get(`${endpoint}/signed-url/${file.name}`)));
     const signed_url = (await this.get(`${endpoint}/signed-url/${file.name}`)).url;
 
-    const headers = new HttpHeaders({'Content-Type': file.type});
-    const req = new HttpRequest( 'PUT', signed_url, file,
-                                  {
-                                    headers: headers,
-                                    reportProgress: true, // track progress
-                                  });
+    const headers = new HttpHeaders({ 'Content-Type': file.type });
+    const req = new HttpRequest('PUT', signed_url, file,
+      {
+        headers: headers,
+        reportProgress: true, // track progress
+      });
 
-    return new Promise ( resolve => {
-        this.http.request(req).subscribe((resp) => {
-        if (resp && (<any> resp).status && (<any> resp).status === 200) {
+    return new Promise(resolve => {
+      this.http.request(req).subscribe((resp) => {
+        if (resp && (<any>resp).status && (<any>resp).status === 200) {
           resolve(this.post(endpoint, payload));
         }
       });
