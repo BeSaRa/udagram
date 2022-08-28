767771ffbf5cbb82b3c3c5078f2790360cb890cc
BeSaRa <besara@windowslive.com>
2022-08-28 10:09:33 +0300

    update bucket name

--- udagram/udagram-frontend/src/app/api/api.service.ts
+++ udagram/udagram-frontend/src/app/api/api.service.ts
@@ -59,7 +59,7 @@ export class ApiService {
     const headers = new HttpHeaders({ 'Content-Type': file.type });
     const req = new HttpRequest('PUT', signed_url, file,
       {
-        headers: headers,
+        headers: headers.append('Authorization', this.httpOptions.headers.get('Authorization')),
         reportProgress: true, // track progress
       });
 
